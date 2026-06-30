0c495f3157eea8afac6f3b90e597d5b41a3f2025
Vandan Baya <vandanbaya@gmail.com>
2026-06-30 08:46:30 +0530

    add real firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -2,10 +2,10 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "PASTE_YOUR_API_KEY_HERE",
-  authDomain: "PASTE_YOUR_AUTH_DOMAIN_HERE",
-  projectId: "PASTE_YOUR_PROJECT_ID_HERE",
-  appId: "PASTE_YOUR_APP_ID_HERE",
+  apiKey: "AIzaSyAiUwopzjFTjJ6yg61-H_P1K-6o_eQ-kmA",
+  authDomain: "photo-gallery-ef340.firebaseapp.com",
+  projectId: "photo-gallery-ef340",
+  appId: "1:762872160785:web:59d6bb52f7e61a1ec5c7b0",
 };
 
 const app = initializeApp(firebaseConfig);
